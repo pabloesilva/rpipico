f279fd0f1d3c793e2ac7df8f81eccf4c0cf7489e
Pablo <pablosilva.ten@gmail.com>
2025-03-20 15:22:22 -0300

    comentario

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ sleep(1)    #le damos tiempo a vREPL
 print("\nLED esta destellando...")
 while True:
     try:
-        led_board.toggle()
-        # led_board.value(not led_board.value())
-        sleep(.5) # sleep 1sec
+        led_board.toggle() #metodo propio de las pico
+        # led_board.value(not led_board.value()) #metodo para ambos, si no le paso argumento lee el estado, sino lo escribe
+        sleep(.1) # sleep 1sec
     except KeyboardInterrupt:
         break
 led_board.off()
